373527b041af342fca0b71d52c9b5f808ff8daa0
David Hall <david@etianen.com>
2009-05-11 17:35:06 +0100

    Added custom user admin options.

--- src/cms/core/admin.py
+++ src/cms/core/admin.py
@@ -28,13 +28,20 @@ class AdminSite(admin.AdminSite):
                          for model in self._registry.keys()]
         return content_types
     
-    def get_permissions(self):
+    def get_permissions(self, request):
         """
         Returns an list of permissions for all models registered with
         this site.
         """
+        user = request.user
         content_types = self.get_content_types()
         permissions = Permission.objects.filter(content_type__in=content_types)
+        if not user.is_superuser:
+            available_permissions = set(user.user_permissions.all())
+            for group in user.groups.all():
+                available_permissions.update(group.permissions.all())
+            available_permission_ids = [permission.id for permission in available_permissions]
+            permissions = permissions.filter(id__in=available_permission_ids)
         permissions = permissions.order_by("content_type__app_label", "content_type__model", "name")
         return permissions
     
@@ -49,6 +56,24 @@ site = AdminSite()
 class UserAdmin(admin.ModelAdmin):
     
     """Admin settings for User models."""
+    
+    fieldsets = ((None, {"fields": ("username", "is_staff", "is_active",)}),
+                 ("Personal information", {"fields": ("first_name", "last_name", "email",)}),
+                 ("Groups", {"fields": ("groups",)}),
+                 ("Advanced permissions", {"fields": ("is_superuser", "user_permissions",),
+                                  "classes": ("collapse",)}),)
+    
+    filter_horizontal = ("user_permissions", "groups",)
+    
+    list_display = ("username", "first_name", "last_name", "email", "is_staff", "is_active",)
+    
+    list_filter = ("is_staff", "is_active",)
+    
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        """Sets up custom foreign key choices."""
+        if db_field.name == "user_permissions":
+            kwargs["queryset"] = self.admin_site.get_permissions(request)
+        return super(UserAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
     
     
 site.register(User, UserAdmin)
@@ -69,7 +94,7 @@ class GroupAdmin(admin.ModelAdmin):
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         """Sets up custom foreign key choices."""
         if db_field.name == "permissions":
-            kwargs["queryset"] = self.admin_site.get_permissions()
+            kwargs["queryset"] = self.admin_site.get_permissions(request)
         return super(GroupAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
     
