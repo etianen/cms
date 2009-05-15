5f07a2e081e4e0e0be35c3c14fe26514401b094a
David Hall <david@etianen.com>
2009-05-15 23:21:54 +0100

    Removed the sludge from manage.py.  This can be trivially replaced by settings the DJANGO_SETTINGS_MODULE environmental variable and using django-admin.py

--- src/example_site/manage.py
+++ src/example_site/manage.py
@@ -6,14 +6,6 @@ For an explanation of this script, and information about all available
 subcommands, please see the Django documentation at:
 
 <http://docs.djangoproject.com/en/dev/ref/django-admin/>
-
-By default, this script will run using the `settings` module found within
-this package.  To use a different flavour of settings file, set the
-environmental variable DJANGO_SETTINGS_FLAVOUR.  For example, setting the
-variable to 'debug' will cause the `settings_debug` module to be loaded instead.
-
-For local development machines, the 'local' flavour of settings file is
-recommended. 
 """
 
 
@@ -21,25 +13,9 @@ import os, sys
 
 from django.core.management import execute_manager
 
-
-# Derive the settings module name from the environmental variables.
-
-
-if "DJANGO_SETTINGS_MODULE" in os.environ:
-    settings_module_name = os.environ["DJANGO_SETTINGS_MODULE"]
-else:
-    settings_module_name = "settings"
-    if "DJANGO_SETTINGS_FLAVOUR" in os.environ:
-        settings_module_name = os.environ.get("DJANGO_SETTINGS_MODULE", "settings")
-        settings_flavour = os.environ["DJANGO_SETTINGS_FLAVOUR"]
-        settings_module_name = "%s_%s" % (settings_module_name, settings_flavour)
-
-
-# Perform a dynamic import of the settings module.
-
-settings_module = __import__(settings_module_name, {}, {}, [""])
+import settings
 
 
 if __name__ == "__main__":
-    execute_manager(settings_module)
+    execute_manager(settings)
 
