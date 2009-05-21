09c1b72ef6b7dbd3ca7286d6abac79419933fbf2
David Hall <david@etianen.com>
2009-05-21 20:04:04 +0100

    The template areas now work in the admin!

--- src/cms/pages/admin.py
+++ src/cms/pages/admin.py
@@ -2,6 +2,7 @@
 
 
 from django import forms
+from django.forms.models import ModelFormMetaclass, media_property
 from django.contrib import admin
 from django.db import models
 
@@ -25,17 +26,25 @@ class PageAdmin(ContentAdmin):
     def get_template_area_fields(self):
         """Returns the names of the template area fields."""
         template_areas = self.model.template_areas
-        template_area_fields = ["content_%s" % area for area in template_areas]
+        template_area_fields = ["%s_content" % area for area in template_areas]
         return template_area_fields
     
     def get_form(self, request, obj=None, **kwargs):
         """Adds the template area fields to the form."""
-        BaseForm = super(PageAdmin, self).get_form(request, obj, **kwargs)
-        form_attrs = dict([(template_area_field, forms.CharField(label=template_area_field.split("_", 1)[1].replace("_", " ").capitalize(),
+        if obj:
+            initial = [content.content for content in obj.content_set.all()]
+        else:
+            initial = ["" for field in self.get_template_area_fields()]
+        form_attrs = dict([(template_area_field, forms.CharField(label=template_area_field.rsplit("_", 1)[0].replace("_", " ").capitalize(),
+                                                                 initial=initial[index],
+                                                                 required=False,
                                                                  widget=HtmlWidget))
-                           for template_area_field in self.get_template_area_fields()])
-        Form = type("%sForm" % self.model.__name__, (BaseForm,), form_attrs)
-        return Form
+                           for index, template_area_field
+                           in enumerate(self.get_template_area_fields())])
+        Form = type("%sForm" % self.model.__name__, (forms.ModelForm,), form_attrs)
+        defaults = {"form": Form}
+        defaults.update(kwargs)
+        return super(PageAdmin, self).get_form(request, obj, **defaults)
     
     def get_fieldsets(self, request, obj=None):
         """Generates the custom content fieldsets."""
@@ -44,6 +53,20 @@ class PageAdmin(ContentAdmin):
         content_fieldset = ("Page content", {"fields": template_area_fields})
         fieldsets = (fieldsets[0], content_fieldset) + fieldsets[1:]
         return fieldsets
+    
+    def save_model(self, request, obj, form, change):
+        """Saves the model and adds its content fields."""
+        obj.save()
+        content_set = obj.content_set.all()
+        for index, template_area_field in enumerate(self.get_template_area_fields()):
+            template_area_content = form.cleaned_data[template_area_field]
+            try:
+                content = content_set[index]
+            except IndexError:
+                content = Content()
+                content.page = obj
+            content.content = template_area_content
+            content.save()
 
     
 site.register(Page, PageAdmin)
