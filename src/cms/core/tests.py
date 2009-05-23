4510cbedc2557a488caba5491402617d7076bbb6
David Hall <david@etianen.com>
2009-05-23 03:38:11 +0100

    Added tests for cached properties.

--- src/cms/core/tests.py
+++ src/cms/core/tests.py
@@ -5,7 +5,67 @@ import datetime, unittest
 
 from django.contrib.contenttypes.models import ContentType
 
+from cms.core.optimizations import cached_property
 from cms.core.serializers import serializer
+
+
+# Test the optimizations.
+
+
+class CachedPropertyTest(object):
+    
+    """Used to test the cached_property optimization."""
+    
+    def __init__(self):
+        self.call_count = 0
+        self._value = "initial"
+        
+    def get_value(self):
+        self.call_count += 1
+        return self._value
+    
+    def set_value(self, value):
+        self._value = value
+        
+    def del_value(self):
+        del self._value
+        
+    value = cached_property(get_value,
+                            set_value,
+                            del_value)
+    
+    
+class TestCachedProperty(unittest.TestCase):
+    
+    """Tests the cached_property optimization."""
+    
+    def setUp(self):
+        """Sets up a CachedPropertyTest object."""
+        self.cached_property_test = CachedPropertyTest()
+    
+    def testGetterIsCached(self):
+        """Tests that calls to getters are cached."""
+        self.cached_property_test.value
+        self.assertEqual(self.cached_property_test.call_count, 1)
+        self.cached_property_test.value
+        self.assertEqual(self.cached_property_test.call_count, 1)
+        
+    def testSetterSetsCache(self):
+        """Tests that calls to setters sets the cache."""
+        self.cached_property_test.value = "changed"
+        self.assertEqual(self.cached_property_test.value, "changed")
+        self.assertEqual(self.cached_property_test.call_count, 0)
+        
+    def testDeleterClearsCache(self):
+        """Tests that calls to deleters clear the cache."""
+        self.cached_property_test.value
+        del self.cached_property_test.value
+        self.assertRaises(AttributeError, lambda: self.cached_property_test.value)
+        self.cached_property_test.value = "changed"
+        self.assertEqual(self.cached_property_test.call_count, 2)
+        
+
+# Test the serialization.
 
 
 class TestSerializer(unittest.TestCase):
@@ -27,7 +87,7 @@ class TestSerializer(unittest.TestCase):
     
     def testSerialize(self):
         """
-        Ensure that encoding and decoding are reversible for a variety of test
+        Tests that encoding and decoding are reversible for a variety of test
         cases.
         """
         for obj in self.examples:
