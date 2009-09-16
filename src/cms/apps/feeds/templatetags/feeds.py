d59049d164f432f1974734e8215d36ac46dbd820
David Hall <david@etianen.com>
2009-09-16 14:07:38 +0100

    Change in twitter feed template.

--- src/cms/apps/feeds/templatetags/feeds.py
+++ src/cms/apps/feeds/templatetags/feeds.py
@@ -1,11 +1,17 @@
 """Template tags used to render feed articles."""
 
 
+import urllib2, datetime
+from xml.dom import minidom
+
 from django import template
+from django.core.cache import cache
+from django.core.serializers.xml_serializer import getInnerText
 from django.utils.dates import MONTHS
+from django.utils import simplejson
 
 from cms.apps.pages.models import Page
-from cms.apps.pages.templatetags import Library 
+from cms.apps.pages.templatetags import Library
 
 
 register = Library()
@@ -45,7 +51,6 @@ def date_field(article):
 def article_archive(context, page, year):
     """Renders the news archive for the given page."""
     page_content = page.content
-    request = context["request"]
     available_months = page_content.articles.dates(page_content.date_field, "month")
     # Generate the news archive.
     article_archive = []
@@ -66,3 +71,50 @@ def article_archive(context, page, year):
     finally:
         context.pop()
     
+
+@register.inclusion_tag("feeds/rss_feed.html")
+def rss_feed(url, count=5):
+    """Generice feed mirroring tag."""
+    cache_key = "feeds.rss_feed:%s" % url
+    feed = cache.get(cache_key)
+    if feed is None:
+        try:
+            response = urllib2.urlopen(url)
+        except urllib2.URLError:
+            feed = {}
+        else:
+            result = minidom.parse(response)
+            items = [{"title": getInnerText(item.getElementsByTagName("title")[0]),
+                      "url": getInnerText(item.getElementsByTagName("link")[0]),
+                      "date": datetime.datetime.strptime(getInnerText(item.getElementsByTagName("pubDate")[0]), "%a, %d %b %Y %H:%M:%S +0000"),
+                      "summary": getInnerText(item.getElementsByTagName("description")[0]),} for item in result.getElementsByTagName("item")]
+            feed = {"items": items,
+                    "title": getInnerText(result.getElementsByTagName("title")[0]),
+                    "url": getInnerText(result.getElementsByTagName("link")[0])}
+        cache.set(cache_key, feed)
+    context = {"feed": feed}
+    return context
+
+
+@register.inclusion_tag("feeds/twitter_feed.html")
+def twitter_feed(user, count=5):
+    """Loads the given Twitter search results and publishes them to the page."""
+    search_url = "http://search.twitter.com/search.json?q=from:%s&rpp=%i" % (user, count)
+    cache_key = "feeds.twitter_feed:%s" % search_url
+    feed = cache.get(cache_key)
+    if feed is None:
+        try:
+            response = urllib2.urlopen(search_url)
+        except urllib2.URLError:
+            feed = {}
+        else:
+            result = simplejson.load(response)
+            items = [{"date": datetime.datetime.strptime(item["created_at"], "%a, %d %b %Y %H:%M:%S +0000"),
+                      "title": item["text"]} for item in result["results"]]
+            feed = {"items": items,
+                    "title": "Twitter / %s" % user,
+                    "url": "http://www.twitter.com/%s" % user}
+        cache.set(cache_key, feed)
+    context = {"feed": feed}
+    return context
+
