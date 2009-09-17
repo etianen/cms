12742d6597324f64464be2939e0a7b5c2ef6e8bb
David Hall <david@etianen.com>
2009-09-17 16:32:55 +0100

    A few tweaks to the new utility libraries.

--- src/cms/apps/utils/remote.py
+++ src/cms/apps/utils/remote.py
@@ -1,57 +1,76 @@
 """Simple library for fetching remote URLs."""
 
 
-import urllib, urllib2
+import urllib, urllib2, BaseHTTPServer
 
 from django.http import HttpResponse
 
 from cms.apps.utils import xml
 
 
-def fetch(url, post_data={}, get_data={}, headers={}, username="", password="", require_success=True, timeout=None):
+class NetworkError(IOError):
+    
+    """Exception thrown when a network error prevents a fetch operation."""
+    
+    
+class HttpError(IOError):
+    
+    """Exception thrown when a HTTP error code prevents a fetch operation."""
+
+
+response_codes = BaseHTTPServer.BaseHTTPRequestHandler.responses
+
+
+def fetch(url, post={}, get={}, headers={}, username="", password="", require_success=True, timeout=None):
     """
     Fetches the given URL, using the parameters provided.
     
     Any get data will automatically be encoded and appended to the URL.
     
     A standard Django HttpResponse will be returned.  If require_success is
-    True (the default), then a non 200 status code will result in an error being
-    thrown.  Setting this to False will result in the error being returned
+    True (the default), then a non-okay status code will result in a HttpError
+    being thrown.  Setting this to False will result in the error being returned
     encoded as a HttpResponse.   
     """
     # Encode the query string, if any.
-    if get_data:
+    if get:
         if "?" in url:
             url += "&"
         else:
             url += "?"
-        query_string = urllib.urlencode(get_data)
-        url += query_string
+        if not isinstance(get, basestring):
+            get = urllib.urlencode(get)
+        url += get
+    # Encode the POST data.
+    if not isinstance(post, basestring):
+        post = urllib.urlencode(post)
     # Create the request.
-    request = urllib2.Request(url, post_data, headers)
+    request = urllib2.Request(url, post, headers)
     # Create an opener.
     opener = urllib2.build_opener()
     # Create an authentication handler.
     if username:
         password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
         password_manager.add_password(None, url, username, password)
-        opener.add_handler(password_manager)
+        auth_handler = urllib2.HTTPBasicAuthHandler(password_manager)
+        opener.add_handler(auth_handler)
     try:
         # Fetch the response.
-        response_content = opener.open(request, timeout=timeout)
+        content = opener.open(request, timeout=timeout)
     except urllib2.HTTPError, ex:
         # Made a connection, but the server was not happy.
         if require_success:
-            raise
-        response_content = ex
-    except urllib2.URLError:
+            raise HttpError, "Error %i (%s): %s" % (ex.code, response_codes.get(ex.code, ["Unknown Error Code", ""])[0], ex.read())
+        content = ex
+        status = ex.code
+    except urllib2.URLError, ex:
         # No network connection - go nuts!
-        raise
+        raise NetworkError, ex.reason
     else:
         status = 200
     # Parse the response.
-    response = HttpResponse(response_content, status=status)
-    for key, value in response_content.info().items():
+    response = HttpResponse(content, status=status)
+    for key, value in content.info().items():
         response[key] = value
     return response
     
--- src/cms/apps/utils/xml.py
+++ src/cms/apps/utils/xml.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import
 
-
+import sys
 import xml.parsers.expat
 
 
@@ -19,35 +19,132 @@ class Element(object):
         self.attrs = attrs or {}
         self.children = []
         
-    def value(self):
+    def get_value(self):
         """Returns the text content of this element."""
         return u"".join(unicode(child) for child in self.children)
     
     def __unicode__(self):
         """Returns the element and all children encoded as XML."""
         attrs = u"".join(u' %s="%s"' % (name, value) for name, value in self.attrs.iteritems())
-        value = self.value()
+        value = self.get_value()
         data = {"name": self.name,
                 "attrs": attrs,
                 "value": value}
         if value:
             return u"<%(name)s%(attrs)s>%(value)s</%(name)s>" % data
         return u"<%(name)s%(attrs)s/>" % data
+    
+    def __str__(self):
+        """Returns an encoded representation of this element."""
+        return unicode(self).encode(sys.getdefaultencoding(), "xmlcharrefreplace")
 
 
 class XML(object):
     
     """Some queryable XML data."""
     
-    __slots__ = ("elements",)
+    __slots__ = ("_elements",)
     
     def __init__(self, elements):
         """Initializes the XML object."""
-        self.elements = elements
+        self._elements = elements
         
+    def get_value(self):
+        """Returns the text content of the first matched element."""
+        return self._elements[0].get_value()
+    
+    value = property(get_value,
+                     doc="The text content of the first matched element.")
+        
+    def get_all_values(self):
+        """Returns a list of the text content of all matched elements."""
+        return [element.get_value() for element in self._elements]
+        
+    all_values = property(get_all_values,
+                          doc="A list of all the text content of all matched elements.")
+        
+    def get_name(self):
+        """Returns the name of the first matched element."""
+        return self._elements[0].name
+    
+    name = property(get_name,
+                    doc="The name of the first matched element.")
+    
+    def get_attrs(self):
+        """Returns the attributes of the first matched element."""
+        return self._elements[0].attrs
+    
+    attrs = property(get_attrs,
+                     doc="The attributes of the first matched element.")
+    
+    def _iter(self, elements, depth=-1):
+        """
+        Recursively iterates over all the given elements and their children.
+        
+        If depth is given, then this will limit the recursion.  A depth of -1
+        (the default) implies infinite recursion.
+        """
+        for element in elements:
+            # Ignore character data.
+            if isinstance(element, basestring):
+                continue
+            yield element
+            if depth != 0:
+                for child in self._iter(element.children, depth - 1):
+                    yield child
+    
+    def _filter(self, elements, args):
+        """Filters the given elements using the given criteria."""
+        args = frozenset(args)
+        matches = set()  # Used to ensure unique results.
+        for element in elements:
+            # Ignore if already matched.
+            if element in matches:
+                continue
+            else:
+                matches.add(element)
+            # Ignore if not in list of valid names.
+            if args and not element.name in args:
+                continue
+            yield element
+    
+    def filter(self, *args):
+        """
+        Returns a filtered version of this XML.
+        
+        This method is passed a series of element names, and only the matched
+        elements will be returned as a new XML object.  All descendents of
+        matched elements will be matched.  If you only wish to match direct
+        descendents, use the `children` method.
+        """
+        return XML(list(self._filter(self._iter(self._elements), args)))
+        
+    def children(self, *args):
+        """
+        Returns a filtered view of direct children of matched elements.
+        
+        Please see the documentation for `filter` for more information.
+        """
+        return XML(list(self._filter(self._iter(self._elements, 1), args)))
+    
     def __unicode__(self):
-        """Returns a unicode representation of this XML data."""
-        return u"".join(unicode(element) for element in self.elements)
+        """Returns a unicode representation the first matched element."""
+        return unicode(self._elements[0])
+    
+    def __str__(self):
+        """Returns an encoded representation of the first matched element."""
+        return str(self._elements[0])
+    
+    def __len__(self):
+        """Returns the number of matched elements."""
+        return len(self._elements)
+    
+    def __getitem__(self, index):
+        """Returns the item at the given index."""
+        elements = self._elements.__getitem__(index)
+        if isinstance(elements, Element):
+            elements = [elements]
+        return XML(elements)
     
 
 class SimpleContentHandler(object):
