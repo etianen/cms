5d46ebf13ee6dfebcc4ca83b04f89428a610c254
Dave Hall <david@etianen.com>
2010-01-05 17:34:10 +0000

    Removed syncsite command and added it to post_syncdb hook.

--- src/cms/apps/utils/management.py
+++ src/cms/apps/utils/management.py
@@ -3,6 +3,8 @@
 
 from django.conf import settings
 from django.contrib.auth.models import Permission
+from django.contrib.sites.models import Site
+from django.contrib.sites.management import create_default_site
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models.signals import post_syncdb
@@ -22,6 +24,25 @@ def create_content_permissions(**kwargs):
         Permission.objects.get_or_create(content_type=content_type,
                                          codename=codename,
                                          defaults={"name": name})
-    
+
+
 post_syncdb.connect(create_content_permissions)
+
+
+def synchronize_current_site(**kwargs):
+    """
+    Changes the fields of the current site to match those in the Django settings
+    file.
+    """
+    create_default_site(**kwargs)
+    site = Site.objects.get_current()
+    site.name = settings.SITE_NAME
+    domain = settings.SITE_DOMAIN
+    if settings.PREPEND_WWW and not domain.startswith("www."):
+        domain = "www." + domain
+    site.domain = domain
+    site.save()
+
+
+post_syncdb.connect(synchronize_current_site)
 
