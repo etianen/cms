7ebdbc1b16914fe82083d3a4cf1f47503d6d2729
Dave Hall <david@etianen.com>
2010-01-07 15:32:01 +0000

    Whitespace adjustment.

--- src/cms/templates/admin/tinymce_link_list.js
+++ src/cms/templates/admin/tinymce_link_list.js
@@ -2,6 +2,7 @@
     TinyMCE external link list.
 */
 
+
 var tinyMCELinkList = new Array(
     {% for title, permalink in links %}
         ["{{title|escapejs}}", "{{permalink|escapejs}}"]{% if not forloop.last %},{% endif %}
