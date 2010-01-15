8e0b78919b15b0cf98e67e160b29bf4dd0dd4b11
Dave Hall <david@etianen.com>
2010-01-15 16:43:58 +0000

    Page admin now uses new pattern tag.

--- src/cms/apps/pages/templatetags/pages_admin.py
+++ src/cms/apps/pages/templatetags/pages_admin.py
@@ -1,52 +1,69 @@
 """Template tags used to adminstrate pages."""
 
 
+from django import template
 from django.core.urlresolvers import reverse
 
 from cms.apps.pages.admin import site, PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE
 from cms.apps.pages.models import Page
-from cms.apps.pages.templatetags import Library
+from cms.apps.pages.templatetags import PatternNode
 
 
-register = Library()
+register = template.Library()
 
 
-@register.inclusion_tag("admin/sitemap.html", takes_context=True)
-def sitemap(context):
-    """Renders the admin sitemap."""
-    request = context["request"]
-    page_admin = site._registry[Page]
-    try:
-        homepage = Page.objects.get_homepage()
-    except Page.DoesNotExist:
-        homepage = None
-    context = {"homepage": homepage,
-               "request": request,
-               "can_add": page_admin.has_add_permission(request),
-               "can_change": page_admin.has_change_permission(request),
-               "create_homepage_url": reverse("admin:pages_page_add") + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE)}
-    return context
+@register.tag
+def sitemap(parser, token):
+    """
+    Renders the admin sitemap::
+    
+        {% sitemap %}
+        
+    """
+    def handler(context):
+        request = context["request"]
+        page_admin = site._registry[Page]
+        try:
+            homepage = Page.objects.get_homepage()
+        except Page.DoesNotExist:
+            homepage = None
+        # Render the sitemap.
+        context.push()
+        try:
+            context.update({"homepage": homepage,
+                            "can_add": page_admin.has_add_permission(request),
+                            "can_change": page_admin.has_change_permission(request),
+                            "create_homepage_url": reverse("admin:pages_page_add") + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE)})
+            return template.loader.render_to_string("admin/sitemap.html", context)
+        finally:
+            context.pop()
+    return PatternNode(parser, token, handler, ("",))
 
 
-@register.inclusion_tag("admin/sitemap_entry.html", takes_context=True)
-def sitemap_entry(context, page):
-    """Renders an entry in the sitemap."""
-    # Extract required context variables.
-    page_admin = site._registry[Page]
-    request = context["request"]
-    # Generate the new context.
-    context = {"page": page,
-               "parent": page.parent,
-               "is_online": page.is_online,
-               "children": page.children,
-               "can_add": page_admin.has_add_permission(request),
-               "can_change": page_admin.has_change_permission(request, page),
-               "can_delete": page_admin.has_delete_permission(request, page),
-               "can_move": page_admin.has_move_permission(request, page),
-               "add_url": reverse("admin:pages_page_add") + "?%s=%s&parent=%i" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE, page.id),
-               "change_url": reverse("admin:pages_page_change", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),
-               "delete_url": reverse("admin:pages_page_delete", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),
-               "request": request}
-    # Render the sitemap entry.
-    return context
+@register.tag
+def sitemap_entry(parser, token):
+    """
+    Renders an entry in the sitemap::
+        
+        {% sitemap_entry page %}
+    
+    """
+    def handler(context, page):
+        # Extract required context variables.
+        page_admin = site._registry[Page]
+        request = context["request"]
+        # Render the entry.
+        context.push()
+        try:
+            context.update({"page": page,
+                            "can_change": page_admin.has_change_permission(request, page),
+                            "can_delete": page_admin.has_delete_permission(request, page),
+                            "can_move": page_admin.has_move_permission(request, page),
+                            "add_url": reverse("admin:pages_page_add") + "?%s=%s&parent=%i" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE, page.id),
+                            "change_url": reverse("admin:pages_page_change", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),
+                            "delete_url": reverse("admin:pages_page_delete", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),})
+            return template.loader.render_to_string("admin/sitemap_entry.html", context)
+        finally:
+            context.pop()
+    return PatternNode(parser, token, handler, ("{page}",))
 
