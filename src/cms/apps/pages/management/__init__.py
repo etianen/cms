4163efc53a1909e2808969bbee08fcb4df7d96ea
Dave Hall <david@etianen.com>
2010-02-01 16:03:11 +0000

    Added fix for synchronizing sites on first syncdb.

--- src/cms/apps/pages/management/__init__.py
+++ src/cms/apps/pages/management/__init__.py
@@ -3,6 +3,7 @@
 
 from django.conf import settings
 from django.contrib.auth.models import Permission
+from django.contrib.sites import models as sites_app
 from django.contrib.sites.models import Site
 from django.contrib.sites.management import create_default_site
 from django.contrib.contenttypes.models import ContentType
@@ -28,12 +29,11 @@ def create_content_permissions(**kwargs):
 post_syncdb.connect(create_content_permissions)
 
 
-def synchronize_current_site(**kwargs):
+def synchronize_current_site(app, **kwargs):
     """
     Changes the fields of the current site to match those in the Django settings
     file.
     """
-    create_default_site(**kwargs)
     site = Site.objects.get_current()
     site.name = settings.SITE_NAME
     domain = settings.SITE_DOMAIN
@@ -43,5 +43,5 @@ def synchronize_current_site(**kwargs):
     site.save()
 
 
-post_syncdb.connect(synchronize_current_site)
+post_syncdb.connect(synchronize_current_site, sender=sites_app)
 
