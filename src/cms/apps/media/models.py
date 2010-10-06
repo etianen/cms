f586800435630f2ca0669cfc54fec63fe3803bf9
Dave Hall <david@etianen.com>
2010-10-06 10:41:05 +0100

    Backported improved upload file escaping

--- src/cms/apps/media/models.py
+++ src/cms/apps/media/models.py
@@ -25,18 +25,29 @@ class Folder(models.Model):
         ordering = ("name",)
     
     
-RE_WHITESPACE = re.compile("\s+")
-    
-    
+RE_WHITESPACE = re.compile(ur"[\s_]+")
+RE_NONALPHA = re.compile(ur"[^a-z0-9\-\.]")
+
+
+def clean_path_component(path):
+    """Clean a component in a filesystem path."""
+    path = path.lower()
+    path = RE_WHITESPACE.sub(u"-", path)
+    path = RE_NONALPHA.sub(u"", path)
+    return path
+
+
 def get_upload_path(instance, filename):
     """
     Generates the upload path for static media files.
-    
+
     This will attempt to prevent filename mangling by prefixing the filename
     with a folder representing the version of the file that was uploaded.
+    It will also attempt to prevent filesystem incompatibilities by sanitizing
+    the filename to lowercase, and removing non-alphanumeric characters.
     """
-    filename = RE_WHITESPACE.sub("-", filename)
-    folder_name = instance._meta.verbose_name_plural.replace(" ", "-")
+    filename = clean_path_component(filename)
+    folder_name = clean_path_component(instance._meta.verbose_name_plural)
     file_version = 1
     while True:
         upload_path = "uploads/%s/%i/%s" % (folder_name, file_version, filename)
