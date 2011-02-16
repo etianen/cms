90afc2a6dae23a6853ef59f8dde12bd6497aecf5
Dave Hall <david@etianen.com>
2011-02-16 15:42:32 +0000

    Removed sitemap caching. Turns out it was broken.

--- src/cms/apps/pages/templatetags/pages_admin.py
+++ src/cms/apps/pages/templatetags/pages_admin.py
@@ -2,7 +2,6 @@
 
 
 from django import template
-from django.core.cache import cache
 from django.core.urlresolvers import reverse
 from django.db.models import Max
 
@@ -23,33 +22,26 @@ def sitemap(parser, token):
         
     """
     def handler(context):
-        # Try to use a cached sitemap.
-        last_modified = Page.objects.aggregate(last_modified=Max("date_modified"))["last_modified"]
-        cache_key = "sitemap:%s" % (str(last_modified).replace(" ", "-"))
-        cached_sitemap = cache.get(cache_key)
-        if cached_sitemap is None:
-            # Perform a conditional import so as not to load the admin module if this
-            # is called by a {% dynamic_tag %}
-            from cms.apps.pages.admin import PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE
-            # Actually do the tag.
-            request = context["request"]
-            page_admin = site._registry[Page]
-            try:
-                homepage = Page.objects.get_homepage()
-            except Page.DoesNotExist:
-                homepage = None
-            # Render the sitemap.
-            context.push()
-            try:
-                context.update({"homepage": homepage,
-                                "can_add": page_admin.has_add_permission(request),
-                                "can_change": page_admin.has_change_permission(request),
-                                "create_homepage_url": reverse("admin:pages_page_add") + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE)})
-                cached_sitemap = template.loader.render_to_string("admin/sitemap.html", context)
-                cache.set(cache_key, cached_sitemap)
-            finally:
-                context.pop()
-        return cached_sitemap
+        # Perform a conditional import so as not to load the admin module if this
+        # is called by a {% dynamic_tag %}
+        from cms.apps.pages.admin import PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE
+        # Actually do the tag.
+        request = context["request"]
+        page_admin = site._registry[Page]
+        try:
+            homepage = Page.objects.get_homepage()
+        except Page.DoesNotExist:
+            homepage = None
+        # Render the sitemap.
+        context.push()
+        try:
+            context.update({"homepage": homepage,
+                            "can_add": page_admin.has_add_permission(request),
+                            "can_change": page_admin.has_change_permission(request),
+                            "create_homepage_url": reverse("admin:pages_page_add") + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE)})
+            return template.loader.render_to_string("admin/sitemap.html", context)
+        finally:
+            context.pop()
     return PatternNode(parser, token, handler, ("",))
 
 
