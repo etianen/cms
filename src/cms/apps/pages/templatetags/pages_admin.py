c241a2385cc3cc07dea7e18a2dc0f95e0665fdb1
Dave Hall <david@etianen.com>
2011-03-21 13:34:00 +0000

    Whitespace fix

--- src/cms/apps/pages/templatetags/pages_admin.py
+++ src/cms/apps/pages/templatetags/pages_admin.py
@@ -29,10 +29,12 @@ def sitemap(context):
     # Render the sitemap.
     context.push()
     try:
-        context.update({"homepage": homepage,
-                        "can_add": page_admin.has_add_permission(request),
-                        "can_change": page_admin.has_change_permission(request),
-                        "create_homepage_url": reverse("admin:pages_page_add") + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE)})
+        context.update({
+            "homepage": homepage,
+            "can_add": page_admin.has_add_permission(request),
+            "can_change": page_admin.has_change_permission(request),
+            "create_homepage_url": reverse("admin:pages_page_add") + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE)
+        })
         return template.loader.render_to_string("admin/sitemap.html", context)
     finally:
         context.pop()
@@ -51,13 +53,15 @@ def sitemap_entry(context, page):
     # Render the entry.
     context.push()
     try:
-        context.update({"page": page,
-                        "can_change": page_admin.has_change_permission(request, page),
-                        "can_delete": page_admin.has_delete_permission(request, page),
-                        "can_move": page_admin.has_move_permission(request, page),
-                        "add_url": reverse("admin:pages_page_add") + "?%s=%s&parent=%i" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE, page.id),
-                        "change_url": reverse("admin:pages_page_change", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),
-                        "delete_url": reverse("admin:pages_page_delete", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),})
+        context.update({
+            "page": page,
+            "can_change": page_admin.has_change_permission(request, page),
+            "can_delete": page_admin.has_delete_permission(request, page),
+            "can_move": page_admin.has_move_permission(request, page),
+            "add_url": reverse("admin:pages_page_add") + "?%s=%s&parent=%i" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE, page.id),
+            "change_url": reverse("admin:pages_page_change", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),
+            "delete_url": reverse("admin:pages_page_delete", args=(page.pk,)) + "?%s=%s" % (PAGE_FROM_KEY, PAGE_FROM_SITEMAP_VALUE),
+        })
         return template.loader.render_to_string("admin/sitemap_entry.html", context)
     finally:
         context.pop()
