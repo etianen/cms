d0a1e76be33b7dc1558a58cc9beed12d350da415
Dave Hall <david@etianen.com>
2011-03-22 18:20:00 +0000

    Added in pages/base.html

--- src/cms/apps/pages/urls.py
+++ src/cms/apps/pages/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = patterns("",
 
     url(r"^$", TemplateView.as_view(
-        template_name = "base.html",
+        template_name = "pages/base.html",
     ), name="index")
     
 )
