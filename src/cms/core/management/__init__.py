7205c881750efb3df9bc1965a22773784d23fb62
Dave Hall <david@etianen.com>
2011-03-23 15:01:03 +0000

    Putting in a hack to make sitemaps unit tests pass.

--- src/cms/core/management/__init__.py
+++ src/cms/core/management/__init__.py
@@ -1,1 +1,12 @@
 """Management routines used by the CMS."""
+
+from django.contrib.sites.models import Site
+from django.db.models.signals import post_delete
+
+
+# HACK: For some reason, the sitemaps unit tests are failing due to a polluted cache.
+def clear_site_cache_on_delete(**kwargs):
+    """Clears the site cache when a site is deleted."""
+    Site.objects.clear_cache()
+
+post_delete.connect(clear_site_cache_on_delete)
