802e3108a89ff2db3a91fd9515ea533a84e0c265
Dave Hall <david@etianen.com>
2011-03-30 17:37:50 +0100

    Added in timestamps to history links

--- src/cms/apps/historylinks/models.py
+++ src/cms/apps/historylinks/models.py
@@ -37,6 +37,14 @@ class HistoryLink(models.Model):
     
     objects = HistoryLinkManager()
     
+    date_created = models.DateTimeField(
+        auto_now_add = True
+    )
+    
+    date_modified = models.DateTimeField(
+        auto_now = True
+    )
+    
     path = models.CharField(
         max_length = 255,
         unique = True,
