363644e7c563522f50159630ff380c9566f7d021
Dave Hall <david@etianen.com>
2011-04-02 17:24:14 +0100

    Added in a favicon handler to remove some pointless database traffic.

--- src/cms/urls.py
+++ src/cms/urls.py
@@ -3,6 +3,7 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, url, include, handler404  # pyline: disable=W0611
 from django.contrib import admin
+from django.views import generic
 
 from cms.core.admin import site as admin_site
 from cms.core.sitemaps import registered_sitemaps
@@ -28,6 +29,11 @@ urlpatterns = patterns("",
     
     # Basic robots.txt.
     url(r"^robots.txt$", "django.views.generic.simple.direct_to_template", kwargs={"template": "robots.txt", "mimetype": "text/plain"}, name="robots_txt"),
+    
+    # Redirect to a sensible favicon location. At the very least, this will keep the 404 page out of Django, saving database hits.
+    url(r"favicon.ico$", generic.RedirectView.as_view(
+        url = settings.STATIC_URL + "site/favicon.ico",
+    ), name="favicon.ico"),
     
 )
 
