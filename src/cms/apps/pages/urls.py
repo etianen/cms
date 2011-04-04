1ba34871bb40ca00b8f812c4f54785f8bc7516dc
Dave Hall <david@etianen.com>
2011-04-04 12:25:48 +0100

    Adding csrf protection to default cms render.

--- src/cms/apps/pages/urls.py
+++ src/cms/apps/pages/urls.py
@@ -1,12 +1,13 @@
 """URLs used by the standard content page."""
 
 from django.conf.urls.defaults import patterns, url
+from django.views.decorators.csrf import csrf_protect
 
 from cms.apps.pages import views
 
 
 urlpatterns = patterns("cms.apps.pages.views",
 
-    url(r"^$", views.ContentIndexView.as_view(), name="index")
+    url(r"^$", csrf_protect(views.ContentIndexView.as_view()), name="index")
     
 )
