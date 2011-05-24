096ab2945bab60012b99512db318e71227ffdb57
Dave Hall <dave@etianen.com>
2011-05-24 16:52:28 +0100

    Fixing minor issue with re-raising exception

--- src/cms/apps/pages/templatetags/thumbnails.py
+++ src/cms/apps/pages/templatetags/thumbnails.py
@@ -48,7 +48,7 @@ def thumbnail(parser, token):
     def handler(context, image, width, height, method=thumbnails.PROPORTIONAL, alias=None):
         try:
             thumbnail = thumbnails.create(image, width, height, method)
-        except IOError:
+        except:
             thumbnail = thumbnails.Thumbnail(image.name, image.url, image.path, width, height)
         if alias:
             context[alias] = thumbnail
