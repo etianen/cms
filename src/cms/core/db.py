1308119499285f9c713f8d98a7e84a846a9097ed
Dave Hall <dave@etianen.com>
2011-06-16 18:08:45 +0100

    Added in postgres table locking.

--- src/cms/core/db.py
+++ src/cms/core/db.py
@@ -13,13 +13,19 @@ def locked(*models, **kwargs):
     database_name = kwargs.pop("database_name", DEFAULT_DB_ALIAS)
     connection = connections[database_name]
     engine_name = settings.DATABASES[database_name]["ENGINE"]
-    if engine_name == "django.db.backends.mysql":
+    if engine_name == "django.db.backends.postgresql_psycopg2":
         cursor = connection.cursor()
-        cursor.execute("LOCK TABLES {0}".format(", ".join("`{0}` WRITE".format(model._meta.db_table) for model in models)))
+        cursor.execute("LOCK TABLE {0} IN ACCESS EXCLUSIVE MODE".format(", ".join(sorted('"{0}"'.format(model._meta.db_table) for model in models))))
+        yield
+    elif engine_name == "django.db.backends.mysql":
+        cursor = connection.cursor()
+        cursor.execute("LOCK TABLES {0}".format(", ".join(sorted("`{0}` WRITE".format(model._meta.db_table) for model in models))))
         try:
             yield
         finally:
             cursor.execute("UNLOCK TABLES")
+    elif engine_name == "django.db.backends.sqlite3":
+        yield  # This has an implicit database-level write lock.
     else:
         warnings.warn("Cannot lock tables for engine {0}".format(engine_name))
         yield
