87b48f671ad7518be46a7b72b50c485fce1ba827
Dave Hall <dave@etianen.com>
2011-09-05 12:25:11 +0100

    Removed class based generic views from CMS core views.

--- src/cms/core/views.py
+++ src/cms/core/views.py
@@ -1,7 +1,6 @@
 """Views used by the CMS."""
 
 from django.shortcuts import render
-from django.views.generic import DetailView
 
 
 def handler500(request):
@@ -9,25 +8,3 @@ def handler500(request):
     response = render(request, "500.html", {})
     response.status_code = 500
     return response
-    
-    
-class PageMixin(object):
-    
-    """A generic view that adds in SEO information for a specified page."""
-    
-    def get_page(self):
-        """Returns the current page."""
-        return self.object
-    
-    def get_context_data(self, **kwargs):
-        """Sets the page's SEO information in the context."""
-        context = super(PageMixin, self).get_context_data(**kwargs)
-        context.update(self.get_page().get_context_data())
-        return context
-        
-        
-class PageDetailView(PageMixin, DetailView):
-    
-    """A generic view that provides a detail view for a page."""
-    
-    slug_field = "url_title"
