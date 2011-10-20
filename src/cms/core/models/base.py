5f594603b542b772abacfdcbc801b9c7cde909c6
Dave Hall <dave@etianen.com>
2011-10-20 12:24:57 +0100

    Removed un-used import.

--- src/cms/core/models/base.py
+++ src/cms/core/models/base.py
@@ -1,6 +1,5 @@
 """Abstract base models used by the page management application."""
 
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.shortcuts import render
