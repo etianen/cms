ef2487c0dbfd12c46dba0043845f62814c504d97
Dave Hall <dave@etianen.com>
2011-12-28 18:19:40 +0000

    Page middleware now renders in process_response phase.

--- src/cms/middleware.py
+++ src/cms/middleware.py
@@ -8,8 +8,7 @@ import sys
 from django.conf import settings
 from django.core import urlresolvers
 from django.core.handlers.base import BaseHandler
-from django.db.transaction import commit_on_success
-from django.http import Http404, HttpResponse
+from django.http import Http404
 from django.views.debug import technical_404_response
 from django.shortcuts import redirect
 
@@ -53,57 +52,44 @@ class PageMiddleware(object):
         self.backend = get_backend()
     
     def process_request(self, request):
-        """
-        Attempts a page dispatch.
-        
-        A page dispatch will be carried out if the URL conf does not contain a
-        match. This a different strategy to the flatpages app, as it is assumed
-        that most requests will require the page dispatch mechanism. If we
-        simply caught 404 responses, then there would be a lot of wasted
-        template rendering.
-        """
+        """Annotates the request with a page backend."""
         # Mount the page backend.
         self.backend.mount(request)
-        # See if the usual resolver will work.
-        resolver = urlresolvers.get_resolver(urlresolvers.get_urlconf())
+            
+    def process_response(self, request, response):
+        """If the response was a 404, attempt to serve up a page."""
+        if response.status_code != 404:
+            return response
+        # Get the current page.
+        page = request.pages.current
+        if page is None:
+            return response
+        script_name = page.get_absolute_url()[:-1]
+        path_info = request.path[len(script_name):]
+        # Dispatch to the content.
         try:
-            # Try to match the given path with the URL conf. If it fails, then
-            # attempt to dispatch to a page.
-            resolver.resolve(request.path)
-        except urlresolvers.Resolver404:
-            # Get the current page.
-            page = request.pages.current
-            if page is None:
-                return None
-            script_name = page.get_absolute_url()[:-1]
-            path_info = request.path[len(script_name):]
-            # Append a slash to match the page precisely.
-            if not path_info and not request.path.endswith("/") and settings.APPEND_SLASH:
-                return redirect(request.path + "/")
-            # Dispatch to the content.
             try:
-                try:
-                    callback, callback_args, callback_kwargs = self.backend.resolve(request, page, path_info)
-                except urlresolvers.Resolver404:
-                    # First of all see if adding a slash will help matters.
-                    if settings.APPEND_SLASH:
-                        new_path_info = path_info + "/"
-                        try:
-                            self.backend.resolve(request, page, path_info)
-                        except urlresolvers.Resolver404:
-                            pass
-                        else:
-                            return redirect(script_name + new_path_info)
-                    return
-                response = commit_on_success(callback)(request, *callback_args, **callback_kwargs)
-                # Validate the response.
-                if not isinstance(response, HttpResponse):
-                    raise ValueError, "The view {0!r} didn't return an HttpResponse object.".format(callback.__name__)
+                callback, callback_args, callback_kwargs = self.backend.resolve(request, page, path_info)
+            except urlresolvers.Resolver404:
+                # First of all see if adding a slash will help matters.
+                if settings.APPEND_SLASH:
+                    new_path_info = path_info + "/"
+                    try:
+                        self.backend.resolve(request, page, new_path_info)
+                    except urlresolvers.Resolver404:
+                        pass
+                    else:
+                        return redirect(script_name + new_path_info)
                 return response
-            except Http404, ex:
-                if settings.DEBUG:
-                    return technical_404_response(request, ex)
-                # Let the normal 404 mechanisms render an error page.
-                return
-            except:
-                return BaseHandler().handle_uncaught_exception(request, resolver, sys.exc_info())
+            response = callback(request, *callback_args, **callback_kwargs)
+            # Validate the response.
+            if not response:
+                raise ValueError, "The view {0!r} didn't return an HttpResponse object.".format(callback.__name__)
+            return response.render()
+        except Http404, ex:
+            if settings.DEBUG:
+                return technical_404_response(request, ex)
+            # Let the normal 404 mechanisms render an error page.
+            return response
+        except:
+            return BaseHandler().handle_uncaught_exception(request, urlresolvers.get_resolver(), sys.exc_info())
