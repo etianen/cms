dbf09629cdf3dcf9e4501ae6aa1fb3cf4756d50c
Dave Hall <dave@etianen.com>
2013-03-01 09:49:35 +0000

    Removing logging setting, since Django 1.5's settings now provide defaults.

--- src/cms/settings.py
+++ src/cms/settings.py
@@ -120,24 +120,3 @@ SEO_PRIORITIES = (
 USE_I18N = False
 
 USE_L10N = True
-
-
-# Logging settings.
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "class": "django.utils.log.AdminEmailHandler"
-        }
-    },
-    "loggers": {
-        "django.request": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-    }
-}
