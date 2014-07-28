42d3eb81e84d56e4693312d0b44da52c4165f43b
Dave Hall <dave@etianen.com>
2014-07-28 15:04:03 +0100

    Using new setuptools rather than distutils, substantially simplifying setup.py script

--- setup.py
+++ setup.py
@@ -1,43 +1,12 @@
 import sys, os
 sys.path.append("src")
+
 from cms import VERSION
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 VERSION_STR = ".".join(str(n) for n in VERSION)
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-cms_dir = 'src/cms'
-
-for dirpath, dirnames, filenames in os.walk(cms_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)[1:]))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
         
 
 # Create the setup config.
@@ -53,8 +22,8 @@ setup(
     package_dir = {
         "": "src",
     },
-    packages = packages,
-    data_files = data_files,
+    packages = find_packages("src"),
+    include_package_data = True,
     scripts = ['src/cms/bin/start_cms_project.py'],
     install_requires = [
         "django",
