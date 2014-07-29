a726fffbf5c47269b30bd7433b7eae81eb29959e
Dave Hall <dave@etianen.com>
2014-07-29 11:24:51 +0100

    Fixing path issue in local settings template.

--- src/cms/project_template/project_name/settings/local.py
+++ src/cms/project_template/project_name/settings/local.py
@@ -21,9 +21,9 @@ TEMPLATE_DEBUG = DEBUG
 
 # Save media files to the user's Sites folder.
 
-MEDIA_ROOT = os.path.expanduser(os.path.join("~/Sites" + SITE_DOMAIN + "media"))
+MEDIA_ROOT = os.path.expanduser(os.path.join("~/Sites", SITE_DOMAIN, "media"))
 
-STATIC_ROOT = os.path.expanduser(os.path.join("~/Sites" + SITE_DOMAIN + "static"))
+STATIC_ROOT = os.path.expanduser(os.path.join("~/Sites", SITE_DOMAIN, "static"))
 
 
 # Use local server.
